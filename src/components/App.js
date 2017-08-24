0df01679e41dd85cc9deaf399e9eb6bd1b77a6f2
Mark <mhazoldt+gh@gmail.com>
2017-08-24 12:08:27 -0700

    earthquake

---
+++ src/components/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import '../styles/App.css';
+import moment from 'moment'
+import EarthquakeList from './EarthquakeList'
+import EarthquakeInfo from './EarthquakeInfo'
+
+
+class App extends Component {
+  render() {
+
+    return (
+      <div className="App">
+        <div className="title">
+          <div className="my-header">
+            Earthquakes!
+          </div>
+        </div>
+        <EarthquakeInfo />
+        <EarthquakeList />
+      </div>
+
+    );
+  }
+}
+
+export default App;
